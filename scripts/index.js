39dadfd94805b7c0fa2f6522be20596757aae1a0
nicky132 <hhq87776153@hotmail.com>
2022-09-24 20:14:35 +0800

    第一次提交

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  // path: 'https://labs.nearpod.com/bodymovin/demo/markus/halloween/markus.json'
+  path: 'data1.json'
+})
